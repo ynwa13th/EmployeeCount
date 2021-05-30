ca14a094303a987c37f9dcb5d3edd9f378563dcb
ynwa13th <32390344+ynwa13th@users.noreply.github.com>
2021-05-30 18:16:48 +0600

    Add files via upload
    backup базы данных EmployeesDB

--- ChangeEmployeeWindow.xaml.cs
+++ ChangeEmployeeWindow.xaml.cs
@@ -21,53 +21,66 @@ namespace EmployeeCount
 
         private void Change_Click(object sender, RoutedEventArgs e)
         {
-            if ((NameBox.Text != NameToChange) ||
-               (SurnameBox.Text != SurnameToChange) ||
-               (PosCombo.SelectedItem.ToString() != SelectedPos) ||
-               (DepCombo.SelectedItem.ToString() != SelectedDep) ||
-               (CommentBox.Text != SelectedComments))
+            try
             {
-                DbConnect.OpenConnection();
-                DbConnect.sql = "UPDATE Employees " +
-                    "SET Name=@Name, Surname=@Surname, Department_Id=(SELECT Id FROM Departments WHERE Name = @DS), " +
-                    "Position_Id=(SELECT Id FROM Positions WHERE Name = @PS) , Comments=@Comment " +
-                    "WHERE Employees.Id = @Id";
-                DbConnect.cmd.CommandType = CommandType.Text;
-                DbConnect.cmd.CommandText = DbConnect.sql;
-                DbConnect.cmd.Parameters.Clear();
-                DbConnect.cmd.Parameters.AddWithValue("@Name", NameBox.Text.ToString());
-                DbConnect.cmd.Parameters.AddWithValue("@Surname", SurnameBox.Text.ToString());
-                DbConnect.cmd.Parameters.AddWithValue("@DS", DepCombo.Text.ToString());
-                DbConnect.cmd.Parameters.AddWithValue("@PS", PosCombo.Text.ToString());
-                DbConnect.cmd.Parameters.AddWithValue("@Comment", CommentBox.Text.ToString());
-                DbConnect.cmd.Parameters.AddWithValue("@Id", SelectedId.ToString());
-                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-                DbConnect.dt = new DataTable();
-                DbConnect.da.Fill(DbConnect.dt);
-                DbConnect.CloseConnection();
-                MessageBox.Show("Сотрудник изменен", "Внимание", MessageBoxButton.OK);
-                Close();
+                if ((NameBox.Text != NameToChange) ||
+                   (SurnameBox.Text != SurnameToChange) ||
+                   (PosCombo.SelectedItem.ToString() != SelectedPos) ||
+                   (DepCombo.SelectedItem.ToString() != SelectedDep) ||
+                   (CommentBox.Text != SelectedComments))
+                {
+                    DbConnect.OpenConnection();
+                    DbConnect.sql = "UPDATE Employees " +
+                        "SET Name=@Name, Surname=@Surname, Department_Id=(SELECT Id FROM Departments WHERE Name = @DS), " +
+                        "Position_Id=(SELECT Id FROM Positions WHERE Name = @PS) , Comments=@Comment " +
+                        "WHERE Employees.Id = @Id";
+                    DbConnect.cmd.CommandType = CommandType.Text;
+                    DbConnect.cmd.CommandText = DbConnect.sql;
+                    DbConnect.cmd.Parameters.Clear();
+                    DbConnect.cmd.Parameters.AddWithValue("@Name", NameBox.Text.ToString());
+                    DbConnect.cmd.Parameters.AddWithValue("@Surname", SurnameBox.Text.ToString());
+                    DbConnect.cmd.Parameters.AddWithValue("@DS", DepCombo.Text.ToString());
+                    DbConnect.cmd.Parameters.AddWithValue("@PS", PosCombo.Text.ToString());
+                    DbConnect.cmd.Parameters.AddWithValue("@Comment", CommentBox.Text.ToString());
+                    DbConnect.cmd.Parameters.AddWithValue("@Id", SelectedId.ToString());
+                    DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                    DbConnect.dt = new DataTable();
+                    DbConnect.da.Fill(DbConnect.dt);
+                    DbConnect.CloseConnection();
+                    MessageBox.Show("Сотрудник изменен", "Внимание", MessageBoxButton.OK);
+                    Close();
+                }
+                else MessageBox.Show("Внесите изменения", "Внимание", MessageBoxButton.OK);
             }
-            else MessageBox.Show("Внесите изменения", "Внимание", MessageBoxButton.OK);
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex, "Ошибка", MessageBoxButton.OK);
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            PosCombo.ItemsSource = MainWindow.PosList;
-            DepCombo.ItemsSource = MainWindow.DepList;
-            SelectedId = Convert.ToString(MainWindow.Selected.Row["Id"]);
-            NameToChange = Convert.ToString(MainWindow.Selected.Row["Name"]);
-            SurnameToChange = Convert.ToString(MainWindow.Selected.Row["Surname"]);
-            SelectedPos = Convert.ToString(MainWindow.Selected.Row["PosName"]);
-            SelectedDep = Convert.ToString(MainWindow.Selected.Row["DepName"]);
-            SelectedComments = Convert.ToString(MainWindow.Selected.Row["Comments"]);
-            NameBox.Text = NameToChange;
-            SurnameBox.Text = SurnameToChange;
-            PosCombo.SelectedItem = SelectedPos;
-            DepCombo.SelectedItem = SelectedDep;
-            CommentBox.Text = SelectedComments;
+            try
+            {
+                PosCombo.ItemsSource = MainWindow.PosList;
+                DepCombo.ItemsSource = MainWindow.DepList;
+                SelectedId = Convert.ToString(MainWindow.Selected.Row["Id"]);
+                NameToChange = Convert.ToString(MainWindow.Selected.Row["Name"]);
+                SurnameToChange = Convert.ToString(MainWindow.Selected.Row["Surname"]);
+                SelectedPos = Convert.ToString(MainWindow.Selected.Row["PosName"]);
+                SelectedDep = Convert.ToString(MainWindow.Selected.Row["DepName"]);
+                SelectedComments = Convert.ToString(MainWindow.Selected.Row["Comments"]);
+                NameBox.Text = NameToChange;
+                SurnameBox.Text = SurnameToChange;
+                PosCombo.SelectedItem = SelectedPos;
+                DepCombo.SelectedItem = SelectedDep;
+                CommentBox.Text = SelectedComments;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex, "Ошибка", MessageBoxButton.OK);
+            }
         }
-
         private void Cansel_Click(object sender, RoutedEventArgs e)
         {
             Close();
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,86 +19,99 @@ namespace EmployeeCount
 
         private void Show_Click(object sender, RoutedEventArgs e)
         {
-            DbConnect.OpenConnection();
-            if (PositionCombo.SelectedIndex == -1 & DepartmentCombo.SelectedIndex == -1)
+            try
             {
-                MessageBox.Show("Не заданы параметры выбора", "Внимание", MessageBoxButton.OK);
-            }
-            else if (PositionCombo.SelectedIndex == -1 & DepartmentCombo.SelectedIndex != -1)
-            {
-
-                DbConnect.sql = "SELECT Employees.Id AS 'Id', Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
-               "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
-               "Positions ON Employees.Position_id = Positions.Id WHERE Departments.Name = @DS;";
-                DbConnect.cmd.CommandType = CommandType.Text;
-                DbConnect.cmd.Parameters.Clear();
-                DbConnect.cmd.Parameters.AddWithValue("@DS", DepartmentCombo.Text.ToString());
-                DbConnect.cmd.CommandText = DbConnect.sql;
-                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-                DbConnect.dt = new DataTable();
-                DbConnect.da.Fill(DbConnect.dt);
-                EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
-            }
-            else if (PositionCombo.SelectedIndex != -1 & DepartmentCombo.SelectedIndex == -1)
-            {
-                DbConnect.sql = "SELECT  Employees.Id AS 'Id',Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
-               "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
-               "Positions ON Employees.Position_id = Positions.Id WHERE Positions.Name = @PS;";
-                DbConnect.cmd.CommandType = CommandType.Text;
-                DbConnect.cmd.Parameters.Clear();
-                DbConnect.cmd.Parameters.AddWithValue("@PS", PositionCombo.Text.ToString());
-                DbConnect.cmd.CommandText = DbConnect.sql;
-                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-                DbConnect.dt = new DataTable();
-                DbConnect.da.Fill(DbConnect.dt);
-                EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
-            }
-            else
-            {
-                DbConnect.sql = "SELECT  Employees.Id AS 'Id',Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
-               "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
-               "Positions ON Employees.Position_id = Positions.Id WHERE Departments.Name = @DS and Positions.Name = @PS;";
-                DbConnect.cmd.CommandType = CommandType.Text;
-                DbConnect.cmd.Parameters.Clear();
-                DbConnect.cmd.Parameters.AddWithValue("@DS", DepartmentCombo.Text.ToString());
-                DbConnect.cmd.Parameters.AddWithValue("@PS", PositionCombo.Text.ToString());
-                DbConnect.cmd.CommandText = DbConnect.sql;
-                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-                DbConnect.dt = new DataTable();
-                DbConnect.da.Fill(DbConnect.dt);
-                if (DbConnect.dt.Rows.Count == 0)
+                DbConnect.OpenConnection();
+                if (PositionCombo.SelectedIndex == -1 & DepartmentCombo.SelectedIndex == -1)
                 {
-                    MessageBox.Show("Сотрудников с выбранными параметрами не существует", "Внимание", MessageBoxButton.OK);
-                    EmployeesGrid.ItemsSource = null;
+                    MessageBox.Show("Не заданы параметры выбора", "Внимание", MessageBoxButton.OK);
+                }
+                else if (PositionCombo.SelectedIndex == -1 & DepartmentCombo.SelectedIndex != -1)
+                {
+
+                    DbConnect.sql = "SELECT Employees.Id AS 'Id', Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
+                   "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
+                   "Positions ON Employees.Position_id = Positions.Id WHERE Departments.Name = @DS;";
+                    DbConnect.cmd.CommandType = CommandType.Text;
+                    DbConnect.cmd.Parameters.Clear();
+                    DbConnect.cmd.Parameters.AddWithValue("@DS", DepartmentCombo.Text.ToString());
+                    DbConnect.cmd.CommandText = DbConnect.sql;
+                    DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                    DbConnect.dt = new DataTable();
+                    DbConnect.da.Fill(DbConnect.dt);
+                    EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
+                }
+                else if (PositionCombo.SelectedIndex != -1 & DepartmentCombo.SelectedIndex == -1)
+                {
+                    DbConnect.sql = "SELECT  Employees.Id AS 'Id',Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
+                   "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
+                   "Positions ON Employees.Position_id = Positions.Id WHERE Positions.Name = @PS;";
+                    DbConnect.cmd.CommandType = CommandType.Text;
+                    DbConnect.cmd.Parameters.Clear();
+                    DbConnect.cmd.Parameters.AddWithValue("@PS", PositionCombo.Text.ToString());
+                    DbConnect.cmd.CommandText = DbConnect.sql;
+                    DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                    DbConnect.dt = new DataTable();
+                    DbConnect.da.Fill(DbConnect.dt);
+                    EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
                 }
                 else
                 {
-                    EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
+                    DbConnect.sql = "SELECT  Employees.Id AS 'Id',Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
+                   "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
+                   "Positions ON Employees.Position_id = Positions.Id WHERE Departments.Name = @DS and Positions.Name = @PS;";
+                    DbConnect.cmd.CommandType = CommandType.Text;
+                    DbConnect.cmd.Parameters.Clear();
+                    DbConnect.cmd.Parameters.AddWithValue("@DS", DepartmentCombo.Text.ToString());
+                    DbConnect.cmd.Parameters.AddWithValue("@PS", PositionCombo.Text.ToString());
+                    DbConnect.cmd.CommandText = DbConnect.sql;
+                    DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                    DbConnect.dt = new DataTable();
+                    DbConnect.da.Fill(DbConnect.dt);
+                    if (DbConnect.dt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("Сотрудников с выбранными параметрами не существует", "Внимание", MessageBoxButton.OK);
+                        EmployeesGrid.ItemsSource = null;
+                    }
+                    else
+                    {
+                        EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
+                    }
                 }
+                DbConnect.CloseConnection();
             }
-            DbConnect.CloseConnection();
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex, "Ошибка", MessageBoxButton.OK);
+            }
         }
 
         public void Refresh_Click(object sender, RoutedEventArgs e)
         {
-
-            DbConnect.OpenConnection();
-            DbConnect.sql = "SELECT Employees.Id AS 'Id', Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
-                "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
-                "Positions ON Employees.Position_id = Positions.Id";
-            DbConnect.cmd.CommandType = CommandType.Text;
-            DbConnect.cmd.CommandText = DbConnect.sql;
-            DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-            DbConnect.dt = new DataTable();
-            DbConnect.da.Fill(DbConnect.dt);
-            EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
-            DbConnect.CloseConnection();
-            PositionCombo.ItemsSource = PosList;
-            DepartmentCombo.ItemsSource = DepList;
-            DepartmentCombo.SelectedIndex = -1;
-            PositionCombo.SelectedIndex = -1;
-            DepLabel.Content = "Выберите подразделение";
-            PosLabel.Content = "Выберите должность";
+            try
+            {
+                DbConnect.OpenConnection();
+                DbConnect.sql = "SELECT Employees.Id AS 'Id', Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
+                    "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
+                    "Positions ON Employees.Position_id = Positions.Id";
+                DbConnect.cmd.CommandType = CommandType.Text;
+                DbConnect.cmd.CommandText = DbConnect.sql;
+                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                DbConnect.dt = new DataTable();
+                DbConnect.da.Fill(DbConnect.dt);
+                EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
+                DbConnect.CloseConnection();
+                PositionCombo.ItemsSource = PosList;
+                DepartmentCombo.ItemsSource = DepList;
+                DepartmentCombo.SelectedIndex = -1;
+                PositionCombo.SelectedIndex = -1;
+                DepLabel.Content = "Выберите подразделение";
+                PosLabel.Content = "Выберите должность";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex, "Ошибка", MessageBoxButton.OK);
+            }
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
@@ -123,62 +136,74 @@ namespace EmployeeCount
 
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            DataRowView emp = (DataRowView)EmployeesGrid.SelectedItem;
-            string Id = Convert.ToString(emp.Row["Id"]);
-            DbConnect.OpenConnection();
-            DbConnect.sql = "DELETE FROM Employees WHERE Id=@Id;";
-            DbConnect.cmd.CommandType = CommandType.Text;
-            DbConnect.cmd.Parameters.Clear();
-            DbConnect.cmd.Parameters.AddWithValue("@Id", Id);
-            DbConnect.cmd.CommandText = DbConnect.sql;
-            DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-            DbConnect.dt = new DataTable();
-            DbConnect.da.Fill(DbConnect.dt);
-            EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
-            DbConnect.CloseConnection();
-            Refresh_Click(null, null);
+            try
+            {
+                DataRowView emp = (DataRowView)EmployeesGrid.SelectedItem;
+                string Id = Convert.ToString(emp.Row["Id"]);
+                DbConnect.OpenConnection();
+                DbConnect.sql = "DELETE FROM Employees WHERE Id=@Id;";
+                DbConnect.cmd.CommandType = CommandType.Text;
+                DbConnect.cmd.Parameters.Clear();
+                DbConnect.cmd.Parameters.AddWithValue("@Id", Id);
+                DbConnect.cmd.CommandText = DbConnect.sql;
+                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                DbConnect.dt = new DataTable();
+                DbConnect.da.Fill(DbConnect.dt);
+                EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
+                DbConnect.CloseConnection();
+                Refresh_Click(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex, "Ошибка", MessageBoxButton.OK);
+            }
         }
-
-
         void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            DbConnect.OpenConnection();
-            DbConnect.sql = "SELECT Name FROM Positions;";
-            DbConnect.cmd.CommandType = CommandType.Text;
-            DbConnect.cmd.CommandText = DbConnect.sql;
-            DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-            using (SqlDataReader reader = DbConnect.cmd.ExecuteReader())
+            try
             {
-                while (reader.Read())
+                DbConnect.OpenConnection();
+                DbConnect.sql = "SELECT Name FROM Positions;";
+                DbConnect.cmd.CommandType = CommandType.Text;
+                DbConnect.cmd.CommandText = DbConnect.sql;
+                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                using (SqlDataReader reader = DbConnect.cmd.ExecuteReader())
                 {
-                    string name = reader.GetString(0);
-                    PosList.Add(name);
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(0);
+                        PosList.Add(name);
+                    }
                 }
+                PositionCombo.ItemsSource = PosList;
+                DbConnect.sql = "SELECT Name FROM Departments;";
+                DbConnect.cmd.CommandType = CommandType.Text;
+                DbConnect.cmd.CommandText = DbConnect.sql;
+                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                using (SqlDataReader reader = DbConnect.cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(0);
+                        DepList.Add(name);
+                    }
+                }
+                DepartmentCombo.ItemsSource = DepList;
+                DbConnect.sql = "SELECT Employees.Id AS 'Id', Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
+                    "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
+                    "Positions ON Employees.Position_id = Positions.Id";
+                DbConnect.cmd.CommandType = CommandType.Text;
+                DbConnect.cmd.CommandText = DbConnect.sql;
+                DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
+                DbConnect.dt = new DataTable();
+                DbConnect.da.Fill(DbConnect.dt);
+                EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
+                DbConnect.CloseConnection();
             }
-            PositionCombo.ItemsSource = PosList;
-            DbConnect.sql = "SELECT Name FROM Departments;";
-            DbConnect.cmd.CommandType = CommandType.Text;
-            DbConnect.cmd.CommandText = DbConnect.sql;
-            DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-            using (SqlDataReader reader = DbConnect.cmd.ExecuteReader())
+            catch (Exception ex)
             {
-                while (reader.Read())
-                {
-                    string name = reader.GetString(0);
-                    DepList.Add(name);
-                }
+                MessageBox.Show("Ошибка: " + ex, "Ошибка", MessageBoxButton.OK);
             }
-            DepartmentCombo.ItemsSource = DepList;
-            DbConnect.sql = "SELECT Employees.Id AS 'Id', Employees.Name AS 'Name', Employees.Surname AS 'Surname', Departments.Name AS 'DepName', Positions.Name AS 'PosName', Employees.Comments AS 'Comments'" +
-                "FROM Departments INNER JOIN Employees ON Departments.Id = Employees.Department_id INNER JOIN " +
-                "Positions ON Employees.Position_id = Positions.Id";
-            DbConnect.cmd.CommandType = CommandType.Text;
-            DbConnect.cmd.CommandText = DbConnect.sql;
-            DbConnect.da = new SqlDataAdapter(DbConnect.cmd);
-            DbConnect.dt = new DataTable();
-            DbConnect.da.Fill(DbConnect.dt);
-            EmployeesGrid.ItemsSource = DbConnect.dt.DefaultView;
-            DbConnect.CloseConnection();
         }
 
         private void DepartmentCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -195,6 +220,6 @@ namespace EmployeeCount
             {
                 PosLabel.Content = "";
             }
-        }       
+        }
     }
 }
